79ec4c7a0af9623e46d00e004c25dace55f71cf5
Ubuntu <ubuntu@ip-172-31-74-158.ec2.internal>
2020-09-06 13:59:17 +0000

    initial commit

---
+++ opencv-docker/download.sh
@@ -0,0 +1,3 @@
+mkdir vendor 
+wget -O vendor/opencv_contrib-4.1.1.tar.gz  https://github.com/opencv/opencv_contrib/archive/4.1.1.tar.gz
+wget -O vendor/opencv-4.1.1.tar.gz  https://github.com/opencv/opencv/archive/4.1.1.tar.gz
