273b9b01ecbdbeaad8cf71300131f73c489e7c68
EC2 Default User <ec2-user@ip-172-16-72-145.ec2.internal>
2020-09-06 14:12:51 +0000

    add pull.sh

---
+++ opencv-docker/pull.sh
@@ -0,0 +1,34 @@
+algorithm_name=opencv-cuda
+
+
+account=$(aws sts get-caller-identity --query Account --output text)
+
+# Get the region defined in the current configuration (default to us-west-2 if none defined)
+region=$(aws configure get region)
+region=${region:-us-west-2}
+echo $region
+
+
+fullname="${account}.dkr.ecr.${region}.amazonaws.com/${algorithm_name}:latest"
+
+# If the repository doesn't exist in ECR, create it.
+
+aws ecr describe-repositories --repository-names "${algorithm_name}" > /dev/null 2>&1
+
+if [ $? -ne 0 ]
+then
+    aws ecr create-repository --repository-name "${algorithm_name}" > /dev/null
+fi
+
+# Get the login command from ECR and execute it directly
+aws ecr get-login-password \
+    --region ${region} \
+| docker login \
+    --username AWS \
+    --password-stdin ${account}.dkr.ecr.${region}.amazonaws.com
+
+
+
+
+docker pull ${fullname}
+
